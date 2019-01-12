093c7ae57a9c2bda42d1acd7f63fa3fb5d6e6502
Marcus Mu <chunkhang@gmail.com>
2019-01-12 23:31:44 +0800

    Add autocomplete plugin

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,29 @@
 #!/usr/bin/env node
 
 const inquirer = require('inquirer')
+const autocomplete = require('inquirer-autocomplete-prompt')
 
 const { choices, actions } = require('../lib')
 
+const includeFilter = (string, pattern) => {
+  return string.toLowerCase().includes(pattern.toLowerCase())
+}
+
+const filterActions = (input) => {
+  return choices.filter((choice) => {
+    // Initial input is undefined, so we do not filter at all for that
+    return input ? includeFilter(choice.name, input) : true
+  })
+}
+
 const main = async () => {
-  const question = {
-    type: 'list',
+  inquirer.registerPrompt('autocomplete', autocomplete)
+  const answers = await inquirer.prompt({
+    type: 'autocomplete',
     name: 'action',
     message: 'Which action to perform?',
-    choices,
-  }
-  const answers = await inquirer.prompt([question])
+    source: async (_, input) => filterActions(input),
+  })
   const action = actions[answers.action]
   action()
 }
