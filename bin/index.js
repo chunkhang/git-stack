bdba076cefdb7a8742f562d66660e0b1dd7cc572
Marcus Mu <chunkhang@gmail.com>
2019-01-12 22:51:41 +0800

    Add symbols to choices

--- bin/index.js
+++ bin/index.js
@@ -5,14 +5,13 @@ const inquirer = require('inquirer')
 const { choices, actions } = require('../lib')
 
 const main = async () => {
-  const answers = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'action',
-      message: 'Which action to perform?',
-      choices,
-    },
-  ])
+  const question = {
+    type: 'list',
+    name: 'action',
+    message: 'Which action to perform?',
+    choices,
+  }
+  const answers = await inquirer.prompt([question])
   const action = actions[answers.action]
   action()
 }
