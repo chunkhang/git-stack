a4b37549b9e649541a842a355a8f93521bff1a43
Marcus Mu <chunkhang@gmail.com>
2019-01-13 00:18:09 +0800

    Add version printing

--- bin/index.js
+++ bin/index.js
@@ -3,8 +3,10 @@
 const inquirer = require('inquirer')
 const autocomplete = require('inquirer-autocomplete-prompt')
 
+const pkg = require('../package.json')
 const { choices, actions } = require('../lib/actions')
 const utils = require('../lib/utils')
+const { VERSION_ARGS } = require('../lib/constants')
 
 const includeFilter = (string, pattern) => {
   return string.toLowerCase().includes(pattern.toLowerCase())
@@ -24,15 +26,22 @@ const filterActions = (input) => {
 }
 
 const main = async () => {
-  inquirer.registerPrompt('autocomplete', autocomplete)
-  const answers = await inquirer.prompt({
-    type: 'autocomplete',
-    name: 'action',
-    message: 'Which action to perform?',
-    source: async (_, input) => filterActions(input),
-  })
-  const action = actions[answers.action]
-  action()
+  const args = process.argv.slice(2)
+  if (args.length >= 1 && VERSION_ARGS.includes(args[0])) {
+    // Print version
+    console.log(pkg.version)
+  } else {
+    // Prompt action to perform
+    inquirer.registerPrompt('autocomplete', autocomplete)
+    const answers = await inquirer.prompt({
+      type: 'autocomplete',
+      name: 'action',
+      message: 'Which action to perform?',
+      source: async (_, input) => filterActions(input),
+    })
+    const action = actions[answers.action]
+    action()
+  }
 }
 
 main()
